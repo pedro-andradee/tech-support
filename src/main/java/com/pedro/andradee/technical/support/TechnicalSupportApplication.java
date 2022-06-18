058fa129fb751a8a250401ca360e372a830a64df
Pedro Andrade <andradephc11@gmail.com>
2022-06-18 11:45:06 -0300

    First commit

---
+++ src/main/java/com/pedro/andradee/technical/support/TechnicalSupportApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.andradee.technical.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalSupportApplication.class, args);
+	}
+
+}
